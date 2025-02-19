e7d4621cb83e032f8e9b903fd5b6abc01e850054
Francesca Sogaro <francesca.sogaro@stellantis.com>
2025-02-19 18:21:05 +0100

    header done

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <header>
+            <div className="header-div">
+                <img src="chef_claude_icon.png" alt="chef claude logo"/>
+                <span>Chef Claude</span>
+            </div>
+        </header>  
+    );
+};
